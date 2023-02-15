063da0ff8d39bac3536f5b52500661a01331f35f
kirillmironov9 <kirill.mironov93@yandex.ru>
2023-02-15 22:31:12 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int amount = 40000; // цена билета
-        int bonus = 20; // кол-во рублей для одной бонусной мили
-
-        System.out.println("Итоговое количество миль: " + amount / bonus + " миль ");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
-
-
-    // Объявляете переменные для входных данных и
-    // параметров программы: одну для хранения
-    // стоимости билета, другую для хранения количества
-    // рублей для одной бонусной мили
-
-    // Рассчитываете количество бонусных миль, используя
-    // значения заведённых переменных. Ответ сохраняете в
-    // новую переменную и выводите на экран
-
 }
